233c1c10e39864d5ea3ad08f53500665d6c7f41d
chebert2 <chebe46@lsu.edu>
2016-10-07 17:04:49 -0500

    Tree items now return a value
    a value is returned to compute lengths of printing a sub tree nodes

--- prog1/Tree/Ident.cs
+++ prog1/Tree/Ident.cs
@@ -13,13 +13,15 @@ namespace Tree
             name = n;
         }
 
-        public override void print(int n)
+        public override int print(int n)
         {
             // There got to be a more efficient way to print n spaces.
             for (int i = 0; i < n; i++)
                 Console.Write(" ");
 
             Console.Write(name);
+
+            return n + name.Length;
         }
         public override bool isSymbol() { return true; }
 
