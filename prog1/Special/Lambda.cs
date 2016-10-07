7c2443d64cbc998644eee66af914a7489eb9e2b5
mmatt26 <mmatt26@lsu.edu>
2016-10-07 09:05:11 -0500

    Update Lambda.cs

--- prog1/Special/Lambda.cs
+++ prog1/Special/Lambda.cs
@@ -14,13 +14,29 @@ namespace Tree
         public override void print(Node t, int n, bool p)
         {
             // TODO: Implement this function.
+	    Node cadr = t.getCdr().getCar();
+	    Node cddr = t.getCdr().getCdr();
 	    for (int i = 0; i<n; i++)
 	    {
 	    	Console.Writeline(" ");
 	    }
 	    if(p == true)
 	    	Console.Writeline("(");
-    									
+            t.getCar().print(n);
+	    Console.Writeline(" "):
+	    
+	    cadr.print(n, false);
+	    n++;
+	    
+	    for(int i = 0; i < n; i++)
+	    	Console.Writeline(" ");
+           cddr.t.getCar().print(n, false);
+	   n--;
+	   
+	   caddr.print(n, true)
+	   Console.Writeline(")");
+	   
+	        									
 	    
   	}
     }
