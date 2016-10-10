4eedfda27805970c6ada4c5b5e6bd8418dd0a3f9
chebert2 <chebe46@lsu.edu>
2016-10-09 19:44:52 -0500

    Updated For Parsing Whole Package
    Good and stable

--- prog1/SPP.cs
+++ prog1/SPP.cs
@@ -39,28 +39,70 @@ public class SPP
         Regular.boolean_first_run = true;
         Regular.lastCall_was_to_cdr = false;
 
+        Parser.pushedBack_extraToken_fromQuoteMark_ = null;
+    Parser.special_type_Has_to_have_non_nil_cdr = false;
+        Parser.last_itertion_Identifier = null;
+        Parser.lastRunHad_Special_id = 0;
+
+        Parser.quote_mark_misc_to_placed_cursor__is_not_new_data = false;
+        Parser.quote_mark__misc_LPAREN__0___2 =  'A';
+    Parser.flip_Past_RightParenthesis_after_tail = false;
+        Parser.start_addit_Ident_needing_End_expression_tail = false;
+
+        Parser.need_to_insert_right_parenthesis = false;
+        Parser.still_need_to_put_in_RPAREN__next_iteration = false;
+        Parser.quote_extension_going = false;
+        Parser.runningStarted_ForSomething = false;
+        Parser.emptyTerm = false;
         // Parse and pretty-print each input expression
 
+        bool firstTimePrinting = false; 
         root = parser.parseExp();
-        while (root != null)
-        {
+        if (root != null)
             root.print(0);
-            
-                 //an error if Console.WriteLine(Parser.number_of_Left_parentheses_extra_over_zero); 
+        while (root != null) 
+        {
+            if (!firstTimePrinting)
+                firstTimePrinting = true;
+            else
+                root.print(0);
+
+
+            //an error if Console.WriteLine(Parser.number_of_Left_parentheses_extra_over_zero); 
             Parser.firstRun = true;
-            Parser.stillReadFirstParenthesis = true;
+            Parser.stillReadFirstParenthesis = false;
             // start new parse line
             Console.WriteLine();
             scanner.numberParentheses_L = 0;
             scanner.numberParentheses_R = 0;
+            scanner.makeANew_Peek = false;
+            scanner.ifPast_lookWasPeek = false;
+            Parser.pushedBack_extraToken_fromQuoteMark_ = null;
+            Parser.quote_mark_misc_to_placed_cursor__is_not_new_data = false;
+            Parser.quote_mark__misc_LPAREN__0___2 = 'A';
+            Parser.special_type_Has_to_have_non_nil_cdr = false;
+            Parser.last_itertion_Identifier = null;
+            Parser.lastRunHad_Special_id = 0;
+
+            Parser.flip_Past_RightParenthesis_after_tail = false;
             Parser.tail_item_A_list = false;
+            Parser.start_addit_Ident_needing_End_expression_tail = false;
             Special.localExpression_ended_case = false;
             Regular.boolean_first_run = true;
             Regular.lastCall_was_to_cdr = false;
 
-            root = parser.parseExp();
-        }
+            Parser.need_to_insert_right_parenthesis = false;
+            Parser.runningStarted_ForSomething = false;
+            Parser.still_need_to_put_in_RPAREN__next_iteration = false;
+            Parser.quote_extension_going = false;
+            Parser.emptyTerm = false;
 
+            root = parser.parseExp();
+            
+        }
+        
+
+        Console.ReadKey();
         return 0;
     }
 }
