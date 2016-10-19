2ca3bd1a56116ade088d16473a1208ad7261ab1f
chebert2 <chebe46@lsu.edu>
2016-10-19 18:06:22 -0500

    improved overall.

--- prog1/Special/Define.cs
+++ prog1/Special/Define.cs
@@ -7,18 +7,18 @@ namespace Tree
     public class Define : Special
     {
         // TODO: Add any fields needed.
-        
+
 
         // TODO: Add an appropriate constructor.
         public Define()
         {
-        
+
         }
 
         public override void print(Node t, int n, bool p)
         {
-         Node cadr = t.getCdr().getCar();
-         Node cddr = t.getCdr().getCdr();
+            Node cadr = t.getCdr().getCar();
+            Node cddr = t.getCdr().getCdr();
             if (p != false)
                 Console.WriteLine("(");
             t.getCar().print(n);
@@ -31,24 +31,28 @@ namespace Tree
             {
                 Console.WriteLine(" ");
                 cadr.print(n, false);
-                Console.Writeline();
-                
+                //   Console.Writeline();
+
                 //should this be outside of current if statement & does it need changing
-                if(cddr != null)
+                if (cddr != null)
                 {
-                    cddr.print(n,false); 
+                    cddr.print(n, false);
                     // Am I supposed to be incrementting n?
                 }
             }
             else
             {
-                for(int i = 0; i < n; i++)
-                    Console.Writeline(" ");
-                t.getCar().print(n);
-                Console.Writeline();
-             }
-            Console.Writeline(")");
-            
+                for (int i = 0; i < n; i++)
+                {
+                   // Console.Writeline(" ");
+                    t.getCar().print(n);
+                    //Console.Writeline();
+
+                    //Console.Writeline(")");
+                }
+            }
+            //return 0;
+
         }
     }
 }
