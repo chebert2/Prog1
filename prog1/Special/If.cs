cda085ed010584513c0860e4f582de681ab18ed8
chebert2 <chebe46@lsu.edu>
2016-10-19 18:06:53 -0500

    improved overall.

--- prog1/Special/If.cs
+++ prog1/Special/If.cs
@@ -18,23 +18,9 @@ namespace Tree
 
         public override void print(Node t, int n, bool p)
         {
-            // TODO: Implement this function
-            
-            Node cddr = t.getCdr().getCdr();
-            Node cadr = t.getCdr().Car();
-            if(p != null)
-                Console.Writeline("(");
-            t.getCar().print(n);
-            cadr.print(0, false);
-            
-            while(cddr != null)
-            {
-                cddr.getCar().print(n);
-                Console.Writeline();
-                cddr = cddr.getCdr(); // may be incorrect, trying to progress through the remaining cddr
-            }
-            cddr.print(n);
-            Console.Writeline(")");
+            // TODO: Implement this function.
+            //return 0;
+        }
     }
 }
 
