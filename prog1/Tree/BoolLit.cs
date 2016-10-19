ac7393a841390b984cf7aecf9b3c7dc3718f2506
chebert2 <chebe46@lsu.edu>
2016-10-19 18:01:50 -0500

    Improved overall.

--- prog1/Tree/BoolLit.cs
+++ prog1/Tree/BoolLit.cs
@@ -13,7 +13,7 @@ namespace Tree
             boolVal = b;
         }
 
-        public override int print(int n)
+        public override void print(int n)
         {
             // There got to be a more efficient way to print n spaces.
             for (int i = 0; i < n; i++)
@@ -23,7 +23,8 @@ namespace Tree
                 Console.Write("#t");
             else
                 Console.Write("#f");
-            return n + 2;
+
+            Special.Indentation_cumulative += 2;
         }
         public override bool isBool()
         {
