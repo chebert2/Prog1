f5b92344dd9b8f8191e09ac68326889df8850565
chebert2 <chebe46@lsu.edu>
2016-10-19 19:38:56 -0500

    improved all in all.

--- prog1/Special/Define.cs
+++ prog1/Special/Define.cs
@@ -17,44 +17,24 @@ namespace Tree
 
         public override void print(Node t, int n, bool p)
         {
-            Node cadr = t.getCdr().getCar();
-            Node cddr = t.getCdr().getCdr();
-            if (p != false)
-                Console.WriteLine("(");
+           
+            if (p == false)
+            {
+                Special.Indentation_cumulative++;
+                Console.Write("(");
+            }
+
             t.getCar().print(n);
-            if (t.getCdr() == null)
+
+            if (t.getCdr() != null)
             {
-                Console.WriteLine(")");
+                Console.Write(" ");
+                Special.Indentation_cumulative++;
+                t.getCdr().print(n, true);
             }
 
-            if (cadr.isPair())
-            {
-                Console.WriteLine(" ");
-                cadr.print(n, false);
-                //   Console.Writeline();
-
-                //should this be outside of current if statement & does it need changing
-                if (cddr != null)
-                {
-                    cddr.print(n, false);
-                    // Am I supposed to be incrementting n?
-                }
-            }
-            else
-            {
-                for (int i = 0; i < n; i++)
-                {
-                   // Console.Writeline(" ");
-                    t.getCar().print(n);
-                    //Console.Writeline();
-
-                    //Console.Writeline(")");
-                }
-            }
-            //return 0;
+         
 
         }
     }
 }
-
-
